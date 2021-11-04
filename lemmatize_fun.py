865707b21bb03dff1712f5876c90852940f712ff
cluzevt <rananjaykunwar23@gmail.com>
2021-11-04 22:27:23 +0530

    df_modification,pos/neg freq,logistic reg

--- lemmatize_fun.py
+++ lemmatize_fun.py
@@ -2,5 +2,5 @@ from pywsd.utils import lemmatize_sentence
 def lemmatize_fun(token_list):
     return lemmatize_sentence(token_list)
 
-print("Here")
-print(lemmatize_fun("Mary leaves the room"))
+#print("Here")
+#print(lemmatize_fun("Mary leaves the room"))
--- preprocess.py
+++ preprocess.py
@@ -21,10 +21,11 @@ from nltk.corpus import stopwords
 
 def preprocess_tweet(tweet):
     stop_words = set(stopwords.words('english'))
+    stop_words.remove("not")
     tweet_token_processed=[]
     pos=[]
     tweet_tokens = tweet.split()
-    print(tweet_tokens)
+    #print(tweet_tokens)
     temp=""
     for i in range(0,len(tweet_tokens)):
         temp=replace_emoji.replace_emoji_fun(tweet_tokens[i])
@@ -48,53 +49,51 @@ def preprocess_tweet(tweet):
                 tweet_token_processed.append(temp[j])
                 if(c):
                     pos.append(len(tweet_token_processed)-1)
-    return (tweet_token_processed,pos)
+    tweet_token_processed = ' '.join(tweet_token_processed)
+    tweet_token_processed = lemmatize_fun.lemmatize_fun(tweet_token_processed)
+    temp = tweet_token_processed[:]
+    tweet_exclamation = []
+    for i in range(0, len(pos)):
+        #tweet_exclamation.append(tweet_token_processed[pos[i]])
+        temp.insert(pos[i]+i,tweet_token_processed[pos[i]])
+    temp.extend(get_tuples.get_tuples(temp))
+    return temp
 
 #tweet="Dew drops fall from the leaves"
-tweet = 'RT @Daniel no!! no I like Bitcoin!!! impordant!!! 👍 https://bitcoin.com #crypto #dogecoin loo##l loo####l'
+#tweet = 'RT @Daniel no!! no I like Bitcoin!!! impordant!!! 👍 https://bitcoin.com #crypto #dogecoin loo##l loo####l'
 #tweet = "Not happy with the way India played... an insipid display of cricket... This side looked like they r on the ground to get beaten..!!!  :'("
 #tweet = "I'm soooooo pisssssed"
-pos=[]
-tweet_token_processed,pos=preprocess_tweet(tweet)
-print(tweet_token_processed)
+#tweet = "I do not like it 1002"
+#tweet_exclamation=[]
+#tweet_token_processed=preprocess_tweet(tweet)
+#print(tweet_token_processed)
+#print(tweet_exclamation)
 #We can remove the stop words, remove the words which are not in dictionary, replace multiple
 #puntuation marks with 1
 #infact we can also do stemming and lemmatization too
 #we can combine all of them i.e one funtion for all of them
-
 #remove the stop words
 #stop word might replace not and thus we should use not to replace the next word with antonym
-
 #we can make n-grams here
-
-
-
 #stop_words = set(stopwords.words('english'))
 #tweet_token_processed=[token for token in tweet_token_processed if not token in stop_words]
-
-
 #lemmatize starting
-
-tweet_token_processed=' '.join(tweet_token_processed)
-
+#tweet_token_processed=' '.join(tweet_token_processed)
 #print(lemmatize_fun.lemmatize_fun(['Mary','leaves', 'the', 'room']))
-
-tweet_token_processed=lemmatize_fun.lemmatize_fun(tweet_token_processed)
-
+#tweet_token_processed=lemmatize_fun.lemmatize_fun(tweet_token_processed)
 #print(tweet_token_processed)
 #tweet_token_processed=tweet_token_processed.split()
-tweet_exclamation=[]
+#tweet_exclamation=[]
+#for i in range(0,len(pos)):
+    #tweet_exclamation.append(tweet_token_processed[pos[i]])
 
-for i in range(0,len(pos)):
-    tweet_exclamation.append(tweet_token_processed[pos[i]])
-
-print(tweet_token_processed)
-print(pos)
-print(tweet_exclamation)
+#print(tweet_token_processed)
+#print(pos)
+#print(tweet_exclamation)
 
 #lemmatize ending
 
-tweet_token_processed.extend(get_tuples.get_tuples(tweet_token_processed))
+#tweet_token_processed.extend(get_tuples.get_tuples(tweet_token_processed))
 
-print(tweet_token_processed)
+#print(tweet_token_processed)
 
