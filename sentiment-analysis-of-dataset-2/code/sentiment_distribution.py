ccbf40ccc7f7252a8fe45f6e479c9129a5981e91
cluzevt <rananjaykunwar23@gmail.com>
2021-11-16 14:32:19 +0530

    adaboost

--- sentiment-analysis-of-dataset-2/code/sentiment_distribution.py
+++ sentiment-analysis-of-dataset-2/code/sentiment_distribution.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from ast import literal_eval
 
-df=pd.read_csv('sa_file_processed.csv', encoding='ISO-8859-1',na_filter=True,na_values='[]', converters={"1": literal_eval})
+df=pd.read_csv('../dataset/Tweets.csv', encoding='ISO-8859-1',na_filter=True,na_values='[]', converters={"1": literal_eval})
 
 #dataset was shuffled before saving
 
 #print(df.groupby('0').count())
-print(df['0'].value_counts())
+print(df['airline_sentiment'].value_counts())
