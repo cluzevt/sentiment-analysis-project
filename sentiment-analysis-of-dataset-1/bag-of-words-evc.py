ac93770c052247560bd0d84825905c7f98d4c72b
cluzevt <rananjaykunwar23@gmail.com>
2021-11-23 11:05:59 +0530

    ensemble

---
+++ sentiment-analysis-of-dataset-1/bag-of-words-evc.py
@@ -0,0 +1,60 @@
+from sklearn.feature_extraction.text import  CountVectorizer
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import  MultinomialNB
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from ast import literal_eval
+
+
+def solve(x,y):
+    X_train, X_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1)
+
+    # print(X_train.head(10)
+    # pd.set_option('display.max_rows', None)
+    # result=X_train.head(100)
+    # print(result)
+    #print(X_train.head(2))
+    #print(y_train.head(2))
+    #print(X_train.dtypes)
+    #print(y_train.dtypes)
+    #return
+
+    md1 = LogisticRegression()
+    md2 = MultinomialNB()
+    md3 = RandomForestClassifier(n_estimators=1000, max_depth=10)
+    md4 = KNeighborsClassifier(n_neighbors=300)
+    md5 = svm.SVC()
+
+    model = VotingClassifier(estimators=[('lr', md1), ('nb', md2), ('rf', md3), ('kn', md4), ('svc', md5)],voting='hard')
+
+    model.fit(X_train, y_train)
+    Y = model.predict(X_test)
+
+
+    print(confusion_matrix(y_test, Y))
+    print(accuracy_score(y_test, Y))
+
+
+#df=pd.read_csv('sa_file_processed1.csv', encoding='ISO-8859-1',na_filter=True,na_values='[]', converters={'1': pd.eval})
+df=pd.read_csv('sa_file_processed1.csv', encoding='ISO-8859-1',na_filter=True,na_values='[]', converters={"1": literal_eval})
+df.dropna(inplace=True)
+df=df.head(10000)
+vectorizer = CountVectorizer(analyzer=lambda x:x)
+#arr=vectorizer.fit_transform(df['1'].tolist()).toarray()
+#lis=[]
+#for e in arr:
+ #   lis.append(e.tolist())
+#df['1']=lis
+arr=vectorizer.fit_transform(df['1'].tolist())
+#print(arr)
+#print(vectorizer.get_feature_names())
+x = pd.DataFrame(arr.todense(), columns=vectorizer.get_feature_names())
+
+#print(x.head(2))
+
+solve(x,df['0'])
